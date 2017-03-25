0ebaa231005dfd1fa1fcbf99f5b6e6abb6993d8d
Peter Szabo <peter.szabo13@gmail.com>
2017-03-25 22:14:25 +0100

    ButterKnife test

--- app/src/main/java/com/example/kotlindemo/kotlinweather/activity/MainActivity.kt
+++ app/src/main/java/com/example/kotlindemo/kotlinweather/activity/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.kotlindemo.kotlinweather.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.example.kotlindemo.kotlinweather.MainApplication
 import com.example.kotlindemo.kotlinweather.R
 import com.example.kotlindemo.kotlinweather.TestClass
@@ -12,17 +15,24 @@ import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var activityComponent: ActivityComponent
-    @Inject lateinit var test: TestClass
+    @BindView(R.id.hello_text) lateinit var helloText: TextView
 
+    private lateinit var activityComponent: ActivityComponent
+    @Inject lateinit var test: TestClass
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
         activityComponent = DaggerActivityComponent.builder()
                 .applicationComponent((application as MainApplication).applicationComponent)
                 .activityModule(ActivityModule(this))
                 .build()
         activityComponent.inject(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        helloText.text = "TEST TEST TEST"
     }
 }
